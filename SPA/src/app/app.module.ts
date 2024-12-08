c4dd54ec01c97aaecb261e6998118b17778e614b
Mateus Maria Gomes Eça de Queiroz Cabral <1220704@isep.ipp.pt>
2024-12-08 22:05:42 +0000

    #5 Finished a better role management

--- SPA/src/app/app.module.ts
+++ SPA/src/app/app.module.ts
@@ -7,7 +7,6 @@ import {CommonModule} from '@angular/common';
 import {ReactiveFormsModule} from '@angular/forms';
 import {FormsModule} from '@angular/forms';
 import {routes} from './app.routes';
-
 import {HomeComponent} from './Components/Dashboards/home/home.component';
 import {AdminMenuComponent} from './Components/Dashboards/admin-home/admin-home.component';
 import {MenuBoxComponent} from './Components/Shared/menu-box/menu-box.component';
@@ -35,46 +34,21 @@ import {SafeUrlPipe} from './Components/LinkTo3dModule/safeUrlPipe/safe-url.pipe
 import {Connect3dComponent} from './Components/LinkTo3dModule/connect3d/connect3d.component';
 import {RegisterPatientProfileComponent} from './Components/PatientManagement/register-patient-profile/register-patient-profile.component';
 import {RegisterPatientComponent} from './Components/Patient/register-patient/register-patient.component';
-import {
-  PatientManagementComponent
-} from './Components/PatientManagement/patientManagement/patient-management.component';
-import {
-  PatientProfileListComponent
-} from './Components/PatientManagement/patient-profile-list/patient-profile-list.component';
-import {
-  PatientProfileDetailsComponent
-} from './Components/PatientManagement/patient-profile-details/patient-profile-details.component';
-import {
-  DeletePatientProfileButtonComponent
-} from './Components/PatientManagement/delete-patient-profile-button/delete-patient-profile-button.component';
+import {PatientManagementComponent} from './Components/PatientManagement/patientManagement/patient-management.component';
+import {PatientProfileListComponent} from './Components/PatientManagement/patient-profile-list/patient-profile-list.component';
+import {PatientProfileDetailsComponent} from './Components/PatientManagement/patient-profile-details/patient-profile-details.component';
+import {DeletePatientProfileButtonComponent} from './Components/PatientManagement/delete-patient-profile-button/delete-patient-profile-button.component';
 import {UpdatePatientAccoutComponent} from "./Components/Patient/update-patient-accout/update-patient-accout.component";
-import {
-  ListOperationRequestComponent
-} from "./Components/OperationRequest/list-operation-request/list-operation-request.component";
-import {
-  DeletePatientAccoutComponent
-} from './Components/Patient/delete-patient-accout1/delete-patient-accout.component';
-import {
-  EditPatientProfileButtonComponent
-} from './Components/PatientManagement/edit-patient-profile-button/edit-patient-profile-button.component';
-import {
-  EditPatientProfileComponent
-} from './Components/PatientManagement/edit-patient-profile/edit-patient-profile.component';
-import {DoctorMenuComponent} from './Components/Dashboards/doctor-home/doctor-home.component';
+import {ListOperationRequestComponent} from "./Components/OperationRequest/list-operation-request/list-operation-request.component";
+import {DeletePatientAccoutComponent} from './Components/Patient/delete-patient-accout1/delete-patient-accout.component';
+import {EditPatientProfileButtonComponent} from './Components/PatientManagement/edit-patient-profile-button/edit-patient-profile-button.component';
+import {EditPatientProfileComponent} from './Components/PatientManagement/edit-patient-profile/edit-patient-profile.component';
+import {StaffHomeComponent} from './Components/Dashboards/staff-home/staff-home.component';
 import { PatientHomeComponent } from './Components/Dashboards/patient-home/patient-home.component';
 import { CheckUserToDeleteComponent } from './Components/Patient/check-user-to-delete/check-user-to-delete.component';
-import {
-  EditOperationRequestComponent
-} from './Components/OperationRequest/edit-operation-request/edit-operation-request.component';
-import {
-  AddOperationRequestComponent
-} from './Components/OperationRequest/add-operation-request/add-operation-request.component';
+import {EditOperationRequestComponent} from './Components/OperationRequest/edit-operation-request/edit-operation-request.component';
+import {AddOperationRequestComponent} from './Components/OperationRequest/add-operation-request/add-operation-request.component';
 import { PatientProfileComponent } from './Components/Patient/patient-profile/patient-profile.component';
-
-
-
-
-
 @NgModule({
   declarations: [
     AppComponent,
@@ -116,7 +90,7 @@ import { PatientProfileComponent } from './Components/Patient/patient-profile/pa
     DeletePatientAccoutComponent,
     EditPatientProfileButtonComponent,
     EditPatientProfileComponent,
-    DoctorMenuComponent,
+    StaffHomeComponent,
     PatientHomeComponent,
     CheckUserToDeleteComponent,
     EditOperationRequestComponent,
