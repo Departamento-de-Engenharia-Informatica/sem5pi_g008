63e3ed4a7afe75c3298a2e8d1f61eb09795b37cf
Mateus Maria Gomes Eça de Queiroz Cabral <1220704@isep.ipp.pt>
2024-12-20 17:56:36 +0000

    Added a simple bootstrap to help in the development of some USs

--- Backend2/src/services/IRepos/IMedicalRecordRepo.ts
+++ Backend2/src/services/IRepos/IMedicalRecordRepo.ts
@@ -4,5 +4,6 @@ import {MedicalRecord} from "../../domain/MedicalRecord/MedicalRecord";
 export default interface IMedicalRecordRepo extends Repo<MedicalRecord>{
     save(medicalRecord: MedicalRecord, medicalRecordId?: string): Promise<MedicalRecord>;
     getAll(): Promise<MedicalRecord[]>;
+    getMedicalRecordById(medicalRecordId: string): Promise<MedicalRecord>;
 
 }
